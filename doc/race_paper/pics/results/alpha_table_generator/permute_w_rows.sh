e627e8da7d3fa811abaca914c71815f2b11cdaf9
Christie Louis Alappat <christiealappatt@gmail.com>
2018-10-11 13:01:59 +0200

    Sort alpha table according to nrows properly

--- doc/race_paper/pics/results/alpha_table_generator/permute_w_rows.sh
+++ doc/race_paper/pics/results/alpha_table_generator/permute_w_rows.sh
@@ -11,8 +11,8 @@ cp $resultFile $permResultFile
 #echo "KEY" > tmp.tmp
 echo $(cut -d"|" -f$sort_col $mtxDetailFile | grep [0-9])|tr " " "\n">tmp.tmp
 paste -d"|" tmp.tmp $resultFile > tmp_file.tmp
-head -n 1 tmp_file.tmp > tmp_file_2.tmp
-(tail -n +2 tmp_file.tmp | sort -s -k1 -n ) >> tmp_file_2.tmp
+#head -n 1 tmp_file.tmp > tmp_file_2.tmp
+(cat tmp_file.tmp | sort -s -k1 -n -t"|") > tmp_file_2.tmp
 #now remove first element
 cut -d"|" -f2- tmp_file_2.tmp > $permResultFile
 	
