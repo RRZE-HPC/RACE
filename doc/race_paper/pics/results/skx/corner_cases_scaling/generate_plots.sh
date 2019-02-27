ab3b93a3b780156b77eef2221ab8949ed1a399c6
Christie Louis Alappat <christiealappatt@gmail.com>
2019-02-27 15:28:13 +0100

    Chapter 7 almost done

--- doc/race_paper/pics/results/skx/corner_cases_scaling/generate_plots.sh
+++ doc/race_paper/pics/results/skx/corner_cases_scaling/generate_plots.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 MATRICES="crankseg_1 Graphene-4096 inline_1 parabolic_fem"
+RLM_copy="-1 25.33 32.61  21.88"
+RLM_load="-1 28.01 36.06 24.19"
 RESULTS="scaling_results"
 OUT="plots"
 
@@ -11,11 +13,25 @@ mkdir -p plots_tmp
 mkdir -p $OUT_NORCM
 mkdir -p $OUT_RCM
 
+ctr=1
+
 for mat in $MATRICES; do
 	for ext in NORCM RCM; do
-		cp plot_template.tex plots_tmp/$mat.tex
+
+		rlm_copy=$(echo $RLM_copy | cut -f $ctr -d " ")
+		rlm_load=$(echo $RLM_load | cut -f $ctr -d " ")
+		template="plot_template.tex" 
+		echo $rlm_copy
+		if [ "$rlm_copy" != "-1" ] ; then
+			template="plot_template_with_rlm.tex"
+		fi
+		cp $template plots_tmp/$mat.tex
 		./substitute.sh "FOLDER" "$PWD/$RESULTS/$ext"  plots_tmp/$mat.tex
 		./substitute.sh "MATRIX_NAME" "$mat" plots_tmp/$mat.tex
+		if [ "$rlm_copy" != "-1" ] ; then
+			./substitute.sh "RLM_COPY" "$rlm_copy" plots_tmp/$mat.tex
+			./substitute.sh "RLM_LOAD" "$rlm_load" plots_tmp/$mat.tex
+		fi
 		cd plots_tmp
 		echo "compiling $mat"
 		pdflatex $mat.tex >> compile.txt
@@ -23,6 +39,7 @@ for mat in $MATRICES; do
 		cd -
 		cp plots_tmp/$mat.pdf $OUT/$ext/.
 	done
+	let ctr=$ctr+1
 done
 
 rm -r plots_tmp
