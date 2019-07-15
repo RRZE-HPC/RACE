df0a0ab19409cfdb0a5583851cb248c12d8e8abc
christiealappatt <christiealappatt@gmail.com>
2019-07-15 14:10:31 +0200

    Remove unnecessary png files also

--- doc/arXiv/pics_with_source/script_arXiv_clean.sh
+++ doc/arXiv/pics_with_source/script_arXiv_clean.sh
@@ -1,1 +1,4 @@
-find . ! \( -path ./matrices/table.tex -prune -o -path ./results/alpha_table_generator/table.tex -prune -o -name "*.png"  -o -name "*.pdf" \) -delete
+find . ! \( -path ./matrices/table.tex -prune -o -path \
+./results/alpha_table_generator/table.tex -prune -o -path \
+./level_construction/FDM_2d_7pt_non_perm.png -prune -o -path \
+./permutation/FDM_2d_7pt_perm.png -prune -o -name "*.pdf" \) -delete
