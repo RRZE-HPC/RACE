b61e16f2187e416945895aad75df9dbf2e2e3ba3
Christie Louis Alappat <christie.alappat@fau.de>
2021-01-19 18:47:10 +0100

    Update perf script

--- examples/matrixPower_perf_test/perf_script.sh
+++ examples/matrixPower_perf_test/perf_script.sh
@@ -1,11 +1,12 @@
 ############# Settings ###################
 
-matrixFolder="/home/vault/unrz/unrz002h/matrix/MatrixPower"
-folder="mtxPower_results"
-powers="1 2 3 4 6 8 10"
+matrixFolder="/home/vault/unrz/unrz002h/matrix/MatrixPower_1"
+folder="mtxPower_results_1"
+powers="1 2 3 4 6 8"
 threads="20"
-nodes="2"
-cacheSizes="15 20 25 35 45"
+nodes="1"
+cacheSizes="1 20 25 35 45 55"
+safetyFactors="1000 1,1000 1,1,1000 1,1,1,1,1000 1,1,1,1,1,1,1000 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,10000"
 RCMs="0 1"
 
 ############ Don't change anything below ############################
@@ -66,29 +67,32 @@ done
 
 
 mtxPower_file="${folder}/mtxPower.csv"
-printf "%20s, %10s, %5s, %5s, %5s, %5s, %5s\n" "# Matrix" "Perf" "Thread" "Power" "CS" "N" "RCM" > ${mtxPower_file}
+printf "%20s, %10s, %5s, %5s, %5s, %5s, %40s, %5s\n" "# Matrix" "Perf" "Thread" "Power" "CS" "N" "SF" "RCM" > ${mtxPower_file}
 
 
 for matrix in $matrix_name; do
     raw_file="${folder}/raw_mtxPower/${matrix}.txt"
     for power in $powers; do
         for thread in $threads; do
-            for node in $nodes; do
-                for cacheSize in $cacheSizes; do
-                    cacheSizePerNode=$(echo $cacheSize/$node | bc -l)
-                    for RCM in $RCMs; do
-                        rcmFlag=""
-                        if [[ $RCM == "1" ]]; then
-                            rcmFlag="-R"
-                        fi
+            for safetyFactor in $safetyFactors; do
+                for node in $nodes; do
+                    for cacheSize in $cacheSizes; do
+                        cacheSizePerNode=$(echo $cacheSize/$node | bc -l)
+                        for RCM in $RCMs; do
+                            rcmFlag=""
+                            if [[ $RCM == "1" ]]; then
+                                rcmFlag="-R"
+                            fi
 
-                        OMP_NUM_THREADS=$thread likwid-pin -c 0-$((thread-1)) ../mtxPower \
-                            -m ${matrixFolder}/${matrix} -c $thread -t 1 \
-                            -i $power -n $node -s $cacheSizePerNode -v $rcmFlag > tmp.txt
+                            RACE_CACHE_VIOLATION_CUTOFF=${safetyFactor} OMP_NUM_THREADS=$thread likwid-pin -c 0-$((thread-1)) ../mtxPower \
+                                -m ${matrixFolder}/${matrix} -c $thread -t 1 \
+                                -i $power -n $node -s $cacheSizePerNode -v $rcmFlag > tmp.txt
 
-                        cat tmp.txt >> ${raw_file}
-                        perf=$(readResult "tmp.txt" "RACE power perf.")
-                        printf "%20s, %10.4f, %5d, %5d, %5d, %5d, %5d\n" ${matrix} ${perf} ${thread} ${power} ${cacheSize} ${node} ${RCM} >> ${mtxPower_file}
+                            cat tmp.txt >> ${raw_file}
+                            perf=$(readResult "tmp.txt" "RACE power perf.")
+                            safetyFactor_wo_comma=$(echo ${safetyFactor} | sed -e "s@,@;@g")
+                            printf "%20s, %10.4f, %5d, %5d, %5.2f, %5d, %40s, %5d\n" ${matrix} ${perf} ${thread} ${power} ${cacheSizePerNode} ${node} ${safetyFactor_wo_comma} ${RCM} >> ${mtxPower_file}
+                        done
                     done
                 done
             done
