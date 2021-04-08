3acfef3755641dc10d1fb1af09d441dfc7d6c830
Christie Louis Alappat <christie.alappat@fau.de>
2021-04-08 14:41:12 +0200

    Add option in script to bypass trad method

--- examples/matrixPower_perf_test/perf_script.sh
+++ examples/matrixPower_perf_test/perf_script.sh
@@ -8,6 +8,7 @@ nodes="1"
 cacheSizes="1 20 25 35 45 55"
 safetyFactors="1000 1,1000 1,1,1000 1,1,1,1,1000 1,1,1,1,1,1,1000 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,10000"
 RCMs="0 1"
+trad="on"
 
 ############ Don't change anything below ############################
 
@@ -34,36 +35,39 @@ echo $matrix_name
 cd -
 
 
-tradPower_file="${folder}/tradPower.csv"
-printf "%20s, %10s, %5s, %5s, %5s\n" "# Matrix" "Perf" "Thread" "Power" "RCM" > ${tradPower_file}
-
 
 function readResult
 {
     cat $1 | grep "$2" | head -n 1 | cut -d "=" -f 2 | cut -d "G" -f1
 }
 
-for matrix in $matrix_name; do
-    raw_file="${folder}/raw_tradPower/${matrix}.txt"
-    for power in $powers; do
-        for thread in $threads; do
-            for RCM in $RCMs; do
-                rcmFlag=""
-                if [[ $RCM == "1" ]]; then
-                    rcmFlag="-R"
-                fi
+if [[ ${trad} == "on" ]]; then
+    tradPower_file="${folder}/tradPower.csv"
+    printf "%20s, %10s, %5s, %5s, %5s\n" "# Matrix" "Perf" "Thread" "Power" "RCM" > ${tradPower_file}
 
-                OMP_NUM_THREADS=$thread likwid-pin -c 0-$((thread-1)) ../tradPower \
-                    -m ${matrixFolder}/${matrix} -c $thread -t 1 \
-                    -i $power -v $rcmFlag > tmp.txt
 
-                cat tmp.txt >> ${raw_file}
-                perf=$(readResult "tmp.txt" "SpMV perf.")
-                printf "%20s, %10.4f, %5d, %5d, %5d\n" ${matrix} ${perf} ${thread} ${power} ${RCM} >> ${tradPower_file}
+    for matrix in $matrix_name; do
+        raw_file="${folder}/raw_tradPower/${matrix}.txt"
+        for power in $powers; do
+            for thread in $threads; do
+                for RCM in $RCMs; do
+                    rcmFlag=""
+                    if [[ $RCM == "1" ]]; then
+                        rcmFlag="-R"
+                    fi
+
+                    OMP_NUM_THREADS=$thread likwid-pin -c 0-$((thread-1)) ../tradPower \
+                        -m ${matrixFolder}/${matrix} -c $thread -t 1 \
+                        -i $power -v $rcmFlag > tmp.txt
+
+                    cat tmp.txt >> ${raw_file}
+                    perf=$(readResult "tmp.txt" "SpMV perf.")
+                    printf "%20s, %10.4f, %5d, %5d, %5d\n" ${matrix} ${perf} ${thread} ${power} ${RCM} >> ${tradPower_file}
+                done
             done
         done
     done
-done
+fi
 
 
 mtxPower_file="${folder}/mtxPower.csv"
