84fca0fff7c116e35da08b5d98c72d67d9c2e94c
Christie Louis Alappat <christie.alappat@fau.de>
2021-05-23 15:51:23 +0200

    Include files of BFS

--- include/traverse.h
+++ include/traverse.h
@@ -1,60 +1,13 @@
 #ifndef RACE_TRAVERSE_H
 #define RACE_TRAVERSE_H
-#include <vector>
-#include <map>
-#include "type.h"
-#include "error.h"
-#include "graph.h"
-#include "levelData.h"
-#include "macros.h"
+#include "config.h"
 
-class Graph;
-
-class Traverse{
-    private:
-        Graph *graph;
-        static std::map<int, LevelData> cachedData;
-        RACE::dist dist;
-        int rangeLo;
-        int rangeHi;
-        int parentIdx;
-        int numRoots;
-        //Size without pure diagonal elements
-        int graphSize;
-
-        int* distFromRoot;
-
-        int* perm;
-        int* invPerm;
-
-        std::vector<std::map<int, std::vector<Range>>> boundaryRange;
-        int totalNodesIncBoundary;
-
-        //Level Details
-        LevelData* levelData;
-        std::vector<std::map<int, std::vector<LevelData*>>> boundaryLevelData;
-
-        std::vector<int> markChildren(int currChild, int currLvl);
-        RACE_error findLevelData(int lower_nrows, int upper_nrows, int totalLevel, LevelData* curLevelData);
-        RACE_error createLevelData();
-        void permuteGraph();
-    public:
-        //constructor
-        Traverse(Graph *graph_, RACE::dist dist, int rangeLo_=0, int rangeHi_=-1, int parentIdx=0, int numRoots=1, std::vector<std::map<int, std::vector<Range>>> boundaryRange_={});
-        ~Traverse();
-        void calculateDistance();
-
-        //deletion of array's after get calling get fns is user's responsibility
-        void getPerm(int  **perm_, int *len);
-        void getInvPerm(int **invPerm_, int *len);
-        LevelData* getLevelData();
-        std::vector<std::map<int, std::vector<LevelData*>>> getBoundaryLevelData();
-};
-
-struct Counter{
-    static int val;
-    static void add();
-    static void reset();
-};
+#ifdef RACE_USE_GAP
+    #include "traverse_GAP.h"
+    #pragma message ( "GAP BFS being used" )
+#else
+    #include "traverse_serial.h"
+    #pragma message ( "Serial BFS being used" )
+#endif
 
 #endif
---
+++ include/traverse_GAP.h
@@ -0,0 +1,70 @@
+#ifndef RACE_TRAVERSE_GAP_H
+#define RACE_TRAVERSE_GAP_H
+#include <vector>
+#include <map>
+#include "type.h"
+#include "error.h"
+#include "graph.h"
+#include "levelData.h"
+#include "macros.h"
+#include "bitmap.h"
+#include "sliding_queue.h"
+
+class Graph;
+
+class Traverse{
+    private:
+        Graph *graph;
+        static std::map<int, LevelData> cachedData;
+        RACE::dist dist;
+        int rangeLo;
+        int rangeHi;
+        int colRangeLo;//the min row where col in rangeLo:rangeHi are present
+        int colRangeHi;//the max row where col in rangeLo:rangeHi are present
+        int parentIdx;
+        int numRoots;
+        //Size without pure diagonal elements
+        int graphSize;
+
+        int* distFromRoot;
+        int* parent;
+
+        int* perm;
+        int* invPerm;
+
+        std::vector<std::map<int, std::vector<Range>>> boundaryRange;
+        int totalNodesIncBoundary;
+        int minBoundary; //min node of all boundaries
+        int maxBoundary; //max node of all boundaries
+        Bitmap* boundary_bm;
+
+        SlidingQueue<int> queue;
+        //Level Details
+        LevelData* levelData;
+        std::vector<std::map<int, std::vector<LevelData*>>> boundaryLevelData;
+
+        //std::vector<int> markChildren(int currChild, int currLvl);
+        void TDStep(int curLvl);
+        RACE_error findLevelData(int lower_nrows, int upper_nrows, int totalLevel, LevelData* curLevelData);
+        RACE_error createLevelData();
+        void permuteGraph();
+    public:
+        //constructor
+        Traverse(Graph *graph_, RACE::dist dist, int rangeLo_=0, int rangeHi_=-1, int parentIdx=0, int numRoots=1, std::vector<std::map<int, std::vector<Range>>> boundaryRange_={});
+        ~Traverse();
+        void calculateDistance();
+
+        //deletion of array's after get calling get fns is user's responsibility
+        void getPerm(int  **perm_, int *len);
+        void getInvPerm(int **invPerm_, int *len);
+        LevelData* getLevelData();
+        std::vector<std::map<int, std::vector<LevelData*>>> getBoundaryLevelData();
+};
+
+struct Counter{
+    static int val;
+    static void add();
+    static void reset();
+};
+
+#endif
